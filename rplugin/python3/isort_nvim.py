9a5c1fc593634986dd864ea7d5b2a040d4d95042
Michał Bielawski <michal.bielawski@apperio.com>
2018-04-09 13:35:41 +0100

    Using the python api directly instead of calling an external method; also, running synchronously.

--- rplugin/python3/isort_nvim.py
+++ rplugin/python3/isort_nvim.py
@@ -1,6 +1,12 @@
-from subprocess import PIPE, Popen
-
 import neovim
+
+
+try:
+    from isort import SortImports
+    isort_imported = True
+except ImportError:
+    isort_imported = False
+
 
 ISORT_COMMAND = 'isort'
 ISORT_OPTIONS = [
@@ -19,34 +25,43 @@ ISORT_OPTIONS = [
 ]
 
 
+def _count_blanks(lines):
+    for i, line in enumerate(reversed(lines)):
+        if line.strip():
+            return i
+
+
 @neovim.plugin
 class IsortNvim:
-
     def __init__(self, nvim):
         self.nvim = nvim
 
     @neovim.command(
-        'Isort', nargs='*', range='%',
-        complete='customlist,IsortCompletions')
+        'Isort',
+        nargs='*',
+        range='%',
+        complete='customlist,IsortCompletions',
+        sync=True,
+    )
     def isort_command(self, args, range):
-        text = self._get_lines(range)
-        output = self._isort(text, *args)
-        lines = output.split('\n')[:-1]
-        self.nvim.current.buffer[range[0] - 1:range[1]] = lines
+        if not isort_imported:
+            self.error('Could not import isort')
+            return
+
+        old_lines = self.nvim.current.buffer[range[0] - 1:range[1]]
+        blanks = _count_blanks(old_lines)
+
+        output = SortImports(file_contents='\n'.join(old_lines)).output
+        new_lines = output.split('\n')
+        new_blanks = _count_blanks(new_lines)
+        if new_blanks > blanks:
+            del new_lines[-(new_blanks - blanks):]
+
+        if old_lines != new_lines:
+            self.nvim.current.buffer[range[0] - 1:range[1]] = new_lines
 
     def error(self, msg):
         self.nvim.err_write('[isort] {}\n'.format(msg))
-
-    def _get_lines(self, range):
-        lines = self.nvim.current.buffer[range[0] - 1:range[1]]
-        return '\n'.join(lines)
-
-    def _isort(self, text, *args):
-        isort_command = self.nvim.vars.get('isort_command', ISORT_COMMAND)
-        isort_args = [isort_command] + list(args) + ['-']
-        with Popen(isort_args, stdin=PIPE, stdout=PIPE, stderr=PIPE) as proc:
-            output, error = proc.communicate(input=text.encode())
-            return output.decode()
 
     @neovim.function('IsortCompletions', sync=True)
     def isort_completions(self, args):
